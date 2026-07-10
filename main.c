2e209e56ef099b5e012aefaab2e8696652d9c658
Ernesto Nematabula <euclydesernestonematabula@gmail.com>
2026-07-10 21:11:46 +0100

    Update main.c

--- main.c
+++ main.c
@@ -2,8 +2,11 @@
 #include <time.h>
 #include "menu.h"
 
-int main() {
-    //srand((unsigned int) time(NULL)); /* inicializa gerador de aleatorios uma unica vez */
+int main() { 
+    /* inicializa gerador de aleatorios uma unica vez, 
+    evita que a cada execucao do programa os numeros gerados aleatoriamente sejam os mesmos*/
+    srand((unsigned int) time(NULL)); 
+   
     menuPrincipal();
     
     return 0;
